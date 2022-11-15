2bdd4aa44735d97125508b46349a7de9a0480ea4
Brian Batista <brianbatista93@gmail.com>
2022-11-15 11:49:21 -0300

    Implementing Linux Launcher (#22)
    [Ticket: #21]

---
+++ Source/Launchers/Linux/LinuxLauncher.cpp
@@ -0,0 +1,28 @@
+#include "Containers/Array.hpp"
+#include "Encoding/AnsiEncoder.hpp"
+#include "Types.hpp"
+
+extern i32 WindmillMain(i32 nArgC, tchar *pArgV[]);
+
+i32 main(i32 nArgC, char *pArgV[])
+{
+    if (setenv("LC_NUMERIC", "en_US", 1) != 0)
+    {
+        fprintf(stderr, "Could not setenv(LC_NUMERIC): errno=%d (%s)", errno, strerror(errno));
+        return errno;
+    }
+
+    TArray<tchar *, TAllocator<i32>> decodedArgV(nArgC);
+
+    auto &decoder = CAnsiEncoder::Get();
+
+    for (i32 i = 0; i < nArgC; ++i)
+    {
+        usize length = strlen(pArgV[i]);
+        usize destLength = decoder.GetLength((const u8 *)pArgV[i], length);
+        decodedArgV[i] = (tchar *)we_malloc(destLength * sizeof(tchar));
+        decoder.Decode(decodedArgV[i], (u8 *)pArgV[i], length);
+    }
+
+    return WindmillMain(decodedArgV.GetSize(), decodedArgV.GetData());
+}
