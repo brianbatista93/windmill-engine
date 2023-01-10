3e22592ec49b58f0cf62e89111fbcc46e89038b4
Brian Batista <brianbatista93@gmail.com>
2023-01-10 09:50:38 -0300

    Implementing base archive class

---
+++ Engine/Source/Modules/Core/Serialization/Archive.cpp
@@ -0,0 +1,51 @@
+#include <memory>
+
+#include "Archive.hpp"
+
+CArchive::~CArchive()
+{
+}
+
+void CArchive::ClearErrors()
+{
+    mHasErrors = false;
+}
+
+u8 *SwapBytes(void *pValue, usize nSize)
+{
+    u8 *pBytes = (u8 *)pValue;
+    usize top = nSize - 1;
+    usize bottom = 0;
+    while (bottom < top)
+    {
+        std::swap(pBytes[top--], pBytes[bottom++]);
+    }
+
+    return pBytes;
+}
+
+void SerializeSwapped(CArchive &archive, void *pValue, usize nSize)
+{
+    if (archive.IsReading())
+    {
+        archive.Serialize(pValue, nSize);
+        pValue = SwapBytes(pValue, nSize);
+    }
+    else
+    {
+        u8 *swappedValue = SwapBytes(pValue, nSize);
+        archive.Serialize(&swappedValue, nSize);
+    }
+}
+
+void CArchive::EndianSerialize(void *pData, usize nSize)
+{
+    if /* UNLIKELY */ (IsByteSwapping())
+    {
+        SerializeSwapped(*this, pData, nSize);
+    }
+    else
+    {
+        Serialize(pData, nSize);
+    }
+}
---
+++ Engine/Source/Modules/Core/Tests/Serialization/ArchiveTest.cc
@@ -0,0 +1,45 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "Serialization/Archive.hpp"
+
+class CArchiveMock : public CArchive
+{
+  public:
+    MOCK_METHOD(void, Serialize, (void *pData, usize nSize), (final));
+};
+
+TEST(Serialization, BasicTypes)
+{
+    CArchiveMock archive;
+
+    u8 u8Value = 0;
+    u16 u16Value = 0;
+    u32 u32Value = 0;
+    u64 u64Value = 0;
+    i8 i8Value = 0;
+    i16 i16Value = 0;
+    i32 i32Value = 0;
+    i64 i64Value = 0;
+    f32 f32Value = 0.0f;
+    f64 f64Value = 0.0;
+    ansi ansiValue = 0;
+    wide wideValue = 0;
+    bool boolValue = false;
+
+    EXPECT_CALL(archive, Serialize).Times(10);
+
+    archive << u8Value;
+    archive << u16Value;
+    archive << u32Value;
+    archive << u64Value;
+    archive << i8Value;
+    archive << i16Value;
+    archive << i32Value;
+    archive << i64Value;
+    archive << f32Value;
+    archive << f64Value;
+    archive << ansiValue;
+    archive << wideValue;
+    archive << boolValue;
+}
